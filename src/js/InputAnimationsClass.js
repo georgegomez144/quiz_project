db479b5cb8ce250f402833d01d03d178a9fdfb3e
georgegomez144k <george144k.dev@gmail.com>
2019-09-18 10:09:45 -0700

    begin input animations class

---
+++ src/js/InputAnimationsClass.js
@@ -0,0 +1,5 @@
+class InputAnimationsClass {
+
+}
+
+export default InputAnimationsClass;
--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,11 @@
 import '../styles/styles.scss';
 import ExampleClass from './ExampleClass';
 import SliderClass from './SliderClass';
+import InputAnimationsClass from './InputAnimationsClass';
 
 // Any JS below runs on page load
 // let example = new ExampleClass();
 // let message = example.getMessage();
 // console.log(message);
 let slider = new SliderClass("#slider_questionaire", {debug: false});
+let inputAnimations = new InputAnimationsClass();
